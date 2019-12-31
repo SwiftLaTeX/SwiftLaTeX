de3c06df26e357512ca88ee73b41e51107e8cba5
elliott <jq.elliott.wen@gmail.com>
2019-12-31 16:41:49 +1300

    stop shipping thirdparty source

--- engine/3rdparty/graphite/contrib/envsetup.sh
+++
@@ -1,5 +0,0 @@
-
-export PRJROOT=`pwd`
-if [ ! -e "$PRJROOT/contrib/envsetup.sh" ]
-then echo "run . ./contrib/envsetup.sh from the root of the graphite2 tree"
-fi
--- engine/3rdparty/graphite/contrib/perl/t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-    use_ok( 'Text::Gr2' );
-}
-
-diag( "Testing Text::Gr2 $Text::Gr2::VERSION, Perl $], $^X" );
