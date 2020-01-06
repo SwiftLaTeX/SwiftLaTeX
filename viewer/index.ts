b83b21b1947ced201d4294ee757528e1584dd22f
elliott <jq.elliott.wen@gmail.com>
2020-01-06 16:07:30 +1300

    ready to fix font sync

--- viewer/index.ts
+++ viewer/index.ts
@@ -2,23 +2,12 @@ import * as fs from "fs";
 import { dvi2html } from "./src";
 
 
-let fonts = "";
-fs.readdirSync('./fonts/output').forEach(file => {
-    let name = file.replace(/.woff/, '');
-    fonts = fonts + `@font-face { font-family: ${name}; src: url('fonts/output/${file}'); }\n`;
-});
-fonts +=  `@font-face { font-family: lmroman10-regular; src: url('lmroman10-regular.otf'); }\n`;
-fonts +=  `@font-face { font-family: lmroman9-regular; src: url('lmroman9-regular.otf'); }\n`;
-
 let bufContent = fs.readFileSync("test.xdv");
 
 let html = "";
 html = html + "<!doctype html>\n";
 html = html + "<html lang=en>\n";
 html = html + "<head>\n";
-html = html + "<style>\n";
-html = html + fonts;
-html = html + "</style>"
 html = html + "</head>\n";
 html = html + '<body>\n';
 html = html + '<div style="position: absolute;">\n';
@@ -26,5 +15,5 @@ html = html + dvi2html(bufContent);
 html = html + '</div>\n';
 html = html + '</body>\n';
 html = html + "</html>\n";
-fs.writeFileSync("index.html", html);2
+fs.writeFileSync("index.html", html);
 console.log("DONE");
