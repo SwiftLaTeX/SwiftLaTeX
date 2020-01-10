e25da19540f2f5d428c577cda20ef1650bdf2ce5
elliott <jq.elliott.wen@gmail.com>
2020-01-11 11:39:46 +1300

    removing texmaker, wont be available until we fix qt

--- texmaker/qmessageboxweb.h
+++
@@ -1,7 +0,0 @@
-#include <QMessageBox>
-
-class QMessageBoxWeb {
-public:
-	static void warning(QWidget *parent, const QString &title, const QString &text);
-	static int warningWith3Buttons(QWidget *parent, const QString &title, const QString &text, const QString &b1,  const QString &b2, const QString &b3, int a0, int a2);
-};
--- texmaker/symbols/invert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for img in *.png
- do 
-  convert "$img" -negate "inverted_$img"
- done 
